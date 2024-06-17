c50a88ddad2a8db16a9a28778b814e2f36b01744
Simulacrum <yajnavalkya@live.com>
2024-06-17 20:45:32 +0200

    working bot

--- qrand.py
+++ qrand.py
@@ -1,8 +1,7 @@
 import discord
 from discord.ext import commands
-
-# import os
-# from dotenv import load_dotenv
+import json
+import random
 
 from private.config import token
 
@@ -12,7 +11,7 @@ intents.typing = False
 intents.presences = False
 
 bot = discord.Client(intents=intents)
-#bot = commands.Bot(command_prefix="!", intents=intents)
+#bot = commands.Bot(command_prefix='!', intents=intents)
 
 @bot.event
 async def on_ready():
@@ -22,9 +21,27 @@ async def on_ready():
 async def on_message(message):
     if message.author == bot.user:
         return
-    
-    if 'rand' in message.content.lower():
-        await message.channel.send(f'{bot.user.name} is at your service! Open me up with a command and I will reveal it all to you!')
+    await message.channel.send('Call me Rand! Call me names!')
 
+    if 'rand' in message.content.lower():
+        await message.channel.send(f'If you call this {bot.user.name} bot names like:\n!absurd\n!begairat\nit will return a quote to prove you right!')
+
+    if 'absurd' in message.content.lower():
+        f = open('absurdity.json')
+        data = json.load(f)
+        key = random.choice(list(data))
+        output1 = key['description']
+        output2 = key['url']
+        await message.channel.send(output1)
+        await message.channel.send(output2)
+
+    if 'begairat' in message.content.lower():
+        f = open('nikah.json')
+        data = json.load(f)
+        key = random.choice(list(data))
+        output1 = key['description']
+        output2 = key['url']
+        await message.channel.send(output1)
+        await message.channel.send(output2)
 
 bot.run(token)
